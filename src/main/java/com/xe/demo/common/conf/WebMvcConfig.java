1ec8b1e19840656ad4843cb7ba79ff410078e8fc
yantao.chen <Aa86358201>
2018-07-12 14:33:15 +0800

    修改静态资源访问路径

---
+++ src/main/java/com/xe/demo/common/conf/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.xe.demo.common.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by yantao.chen on 2018-07-12.
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/**").addResourceLocations("file:E:/service/upload/");
+    }
+
+}
